e47a5e6a186d926fe62843d4a2c2461fa701a9b6
Valetina2903 <90145197+Valetina2903@users.noreply.github.com>
2023-05-03 16:51:22 +0400

    Update 2.py

--- 2.py
+++ 2.py
@@ -12,15 +12,15 @@ def rolling_a_dice():
 def playing_a_game():
     rolling = rolling_a_dice()
     if rolling == 7 or rolling == 11:
-        print("You are a winner! The sum of the dice is ", rolling)
+        print("You are a winner! The sum of the dice is ", rolling) #if it is 7 or 11 you ar a winner
         return True
 
     elif rolling == 2 or rolling == 3 or rolling == 12:
-        print ("You are losing! The sum of dice is ", rolling)
+        print ("You are losing! The sum of dice is ", rolling) #if it is 2, or 3,12 you are losing
         return False
 
     elif rolling == 4 or rolling ==  5 or rolling == 6 or rolling == 8 or rolling == 9 or rolling == 10:
-        print("The sum of dice is", rolling, "This is your goal nuber. ")
+        print("The sum of dice is", rolling, "This is your goal nuber. ") #other numbers are goal numbers
         goal = rolling
         while True:
             rolling = rolling_a_dice()
